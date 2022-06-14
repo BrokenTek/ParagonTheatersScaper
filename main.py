8f7827c490b81d628376405c67523534b376cdd4
Carson Pribble <61444895+BrokenTek@users.noreply.github.com>
2022-06-14 15:18:15 -0400

    main3

--- main.py
+++ main.py
@@ -28,50 +28,32 @@ def writeToFile(lyst):
 		f.write(str(lyst[i].getSoldSeats()))
 		f.write("\n")
 		f.write("\n")
-	
-	
-def main():
-	list_of_movies = []
 
-	# Current chromedriver version is v102 for Chrome version 102
-	driver = webdriver.Chrome(".\chromedriver.exe")
-	url = "https://www.paragontheaters.com/"
-	driver.get(url)
-
-	# go to the Cary Parkside Location
+def getCaryLocation(driver):
 	locations = driver.find_element(By.CLASS_NAME, "subNavContainer")
 	locations.click()
 	locations1 = driver.find_elements(By.ID, "theaterBox")
 	parkside = locations1[5]
-	parkside.click()
+	return parkside
 
-	parkside_url = driver.current_url
-	print(parkside_url)
-
-	movies_obj_list = driver.find_elements(By.ID, "showtimes_movie")
-
+def getMovieDataLists(movie_box):
 	list_of_lists = []
-
-	for i in range(len(movies_obj_list)):
-		sections_obj_list = movies_obj_list[i].find_elements(By.ID, "comment")
-		for j in range(len(sections_obj_list)):
-			seat_type = sections_obj_list[j].find_element(By.TAG_NAME, "div").text.split()
+	for i in range(len(movie_box)):
+		times_box = movie_box[i].find_elements(By.ID, "comment")
+		for j in range(len(times_box)):
+			seat_type = times_box[j].find_element(By.TAG_NAME, "div").text.split()
 			first_word = seat_type[0]
-			last_word = seat_type[-1]
 			inner_list = []
 			if first_word == "LUX":
 				inner_list.append(i)
 				inner_list.append(j)
-				#list_of_lists.append(inner_list)
-				#for h in range(len())
-				num_times = len(sections_obj_list[j].find_elements(By.CLASS_NAME, "view"))
+				num_times = len(times_box[j].find_elements(By.CLASS_NAME, "view"))
 				inner_list.append(num_times)
 				list_of_lists.append(inner_list)
+	return list_of_lists
 
-	driver.quit()
-
-
-	# POSSIBLE FIX BUT CREATED AN ERROR. MUST RUN scapeSeats on each time not just the first time in each section of each movie -_-
+def populateMovieObjList(list_of_lists, parkside_url):
+	list_of_movies = []
 	for movie in list_of_lists:
 		for time in range(movie[2]):
 			movie[2] -= 1
@@ -81,12 +63,45 @@ def main():
 				list_of_movies.append(movie_obj)
 			except:
 				pass
+	return list_of_movies
 
-	for i in list_of_movies:
+def main():
+	
+	options = webdriver.ChromeOptions()
+	options.add_argument('--ignore-certificate-errors')
+
+	# Current chromedriver version is v102 for Chrome version 102
+	driver = webdriver.Chrome(".\chromedriver.exe", chrome_options=options)
+	url = "https://www.paragontheaters.com/"
+	driver.get(url)
+
+	# Go to the Cary Parkside Location
+	getCaryLocation(driver).click()
+
+	parkside_url = driver.current_url
+	print("Working URL:",parkside_url)
+
+	# Find each showtime and create list
+	movie_box = driver.find_elements(By.ID, "showtimes_movie")
+
+	# Create list of movie data
+	list_of_lists = getMovieDataLists(movie_box)
+
+	driver.quit()
+
+	# Create list of Movie objects
+	list_of_movie_objs = populateMovieObjList(list_of_lists, parkside_url)
+
+	# Print notice of success
+	print("\nSuccess!\n")
+
+	# Print movie data to console
+	for i in list_of_movie_objs:
 		print(i)
 		print()
-		
-	writeToFile(list_of_movies)	
+
+	# Write movie data to text file in order
+	writeToFile(list_of_movie_objs)
 
 if __name__ == "__main__":
 	main()
